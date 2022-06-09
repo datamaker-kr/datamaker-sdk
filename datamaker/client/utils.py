392822ac21a396a8215d86367a50e89ddae03e82
MinJun jo <minjun.jo@datamaker.io>
2022-06-09 18:01:08 +0900

    import dataset add batch size

--- datamaker/client/utils.py
+++ datamaker/client/utils.py
@@ -8,3 +8,7 @@ def get_default_url_conversion(url_conversion, **kwargs):
     if url_conversion:
         defaults.update(url_conversion)
     return defaults
+
+
+def dataset_batch(dataset, batch_size):
+    return [dataset[data:data + batch_size] for data in range(0, len(dataset), batch_size)]
