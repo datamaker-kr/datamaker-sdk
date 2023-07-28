da1dd3e46494e2b457017b75e656699cd14039d5
유은정 <eunjung.yu@datamaker.io>
2023-07-28 17:08:34 +0900

    groups, batch_size 가져오는 부분 수정, DatasetClientMixin 인스턴스로 가져오기

--- datamaker/plugins/imports.py
+++ datamaker/plugins/imports.py
@@ -6,15 +6,15 @@ from datamaker.plugins import BasePlugin
 
 
 class BaseImport(BasePlugin):
-    def __init__(self, dataset_id, project_id, storage_id, storage_path, allowed_extensions, **kwargs):
+    def __init__(self, dataset_id, project_id, storage_id, storage_path, allowed_extensions, groups, batch_size, **kwargs):
         super().__init__(**kwargs)
         self.dataset_id = dataset_id
         self.project_id = project_id
         self.storage_id = storage_id
         self.storage_path = storage_path
         self.allowed_extensions = allowed_extensions
-        self.groups = kwargs['groups']
-        self.batch_size = 500 if not kwargs['batch_size'] else kwargs['batch_size']
+        self.groups = groups
+        self.batch_size = 500 if not batch_size else batch_size
 
     # TODO 나중에는 get_pathlib 통해서 가져올 것, 현재는 /mnt/projects/에서 가져온다 가정
     def get_files(self):
@@ -54,7 +54,7 @@ class BaseImport(BasePlugin):
 
     def import_dataset(self):
         dataset = self.prepare_dataset()
-        DatasetClientMixin.import_dataset(
+        DatasetClientMixin().import_dataset(
             self.dataset_id,
             dataset,
             project_id=self.project_id,
