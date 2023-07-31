ba803b4e9810c450e65cb502f10cdfbb99824742
유은정 <eunjung.yu@datamaker.io>
2023-07-31 17:59:51 +0900

    test 후 버그 수정

--- datamaker/plugins/imports.py
+++ datamaker/plugins/imports.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from datamaker.client import DatasetClientMixin
-
+from datamaker.client import ClientError
 from datamaker.plugins import BasePlugin
 
 
 class BaseImport(BasePlugin):
-    def __init__(self, dataset_id, project_id, storage_id, storage_path, allowed_extensions, groups, batch_size, **kwargs):
+    def __init__(self, dataset_id, project_id, storage_id, storage_path, allowed_extensions, groups,
+                 batch_size, **kwargs):
         super().__init__(**kwargs)
         self.dataset_id = dataset_id
         self.project_id = project_id
@@ -19,16 +19,14 @@ class BaseImport(BasePlugin):
     # TODO 나중에는 get_pathlib 통해서 가져올 것, 현재는 /mnt/projects/에서 가져온다 가정
     def get_files(self):
         files = {}
-        for allowed_extension in self.allowed_extensions:
-            for file_type, extensions in allowed_extension.items():
-                files[file_type] = []
-                for extension in extensions:
-                    paths_image = Path(f'/mnt/projects/{self.storage_path}').rglob(f'*.{extension}')
-                    for index, path in enumerate(paths_image):
-                        if path.parent.name == '@eaDir' or path.parent.parent.name == '@eaDir':
-                            continue
-
-                        files[file_type].append(path)
+        for file_type, extensions in self.allowed_extensions.items():
+            files[file_type] = []
+            for extension in extensions:
+                paths_image = Path(f'/mnt/projects/{self.storage_path}').rglob(f'*.{extension}')
+                for index, path in enumerate(paths_image):
+                    if path.parent.name == '@eaDir' or path.parent.parent.name == '@eaDir':
+                        continue
+                    files[file_type].append(path)
         return files
 
     def prepare_dataset(self):
@@ -53,10 +51,16 @@ class BaseImport(BasePlugin):
         return dataset
 
     def import_dataset(self):
+        response = {'status': 'success'}
         dataset = self.prepare_dataset()
-        DatasetClientMixin().import_dataset(
-            self.dataset_id,
-            dataset,
-            project_id=self.project_id,
-            batch_size=self.batch_size
-        )
+        try:
+            self.logger.client.import_dataset(
+                self.dataset_id,
+                dataset,
+                project_id=self.project_id,
+                batch_size=self.batch_size
+            )
+        except ClientError as e:
+            response['status'] = str(e)
+
+        return response
