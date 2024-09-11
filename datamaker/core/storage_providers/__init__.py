267f8101c2db28de59cd1c4fc379ee6f43505dbf
유은정 <eunjung.yu@datamaker.io>
2024-09-11 16:39:13 +0900

    label 추가

--- datamaker/core/storage_providers/__init__.py
+++ datamaker/core/storage_providers/__init__.py
@@ -1,8 +1,13 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from typing import Optional
 
 
 class BaseProvider(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def label(self):
+        pass
+
     def __init__(self, configuration: Optional[dict] = None):
         if configuration is None:
             configuration = {}
