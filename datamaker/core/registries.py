1a840abde7e43aeb4c22b465dfb8eeea5785c656
유은정 <eunjung.yu@datamaker.io>
2024-09-11 16:59:00 +0900

    E501 오류 해결

--- datamaker/core/registries.py
+++ datamaker/core/registries.py
@@ -1,6 +1,6 @@
 REGISTRIES = {
     'STORAGE_PROVIDERS': {
-        'amazon_s3': 'datamaker.core.storage_providers.amazon_s3.AmazonS3StorageProvider',
+        'amazon_s3': 'datamaker.core.storage_providers.amazon_s3.AmazonS3StorageProvider',  # noqa: E501
         'file_system': 'datamaker.core.storage_providers.file_system.FileSystemStorageProvider',  # noqa: E501
     },
 }
